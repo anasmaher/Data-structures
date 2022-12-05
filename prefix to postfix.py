731c3269ce853a352961d19a78922e6cd818fdc8
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2022-12-05 22:35:31 +0200

    Create prefix to postfix.py

---
+++ prefix to postfix.py
@@ -0,0 +1,14 @@
+s = "*-A/BC-/AKL"
+stack = []
+
+operators = set(['+', '-', '*', '/', '^'])
+s = s[::-1]
+for i in s:
+	if i in operators:
+		a = stack.pop()
+		b = stack.pop()
+		temp = a+b+i
+		stack.append(temp)
+	else:
+		stack.append(i)
+print(*stack)
