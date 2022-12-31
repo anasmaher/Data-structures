435b94eae515c94fe48cc949fbef4131053a7100
Anas Maher <91443394+anasmaher@users.noreply.github.com>
2023-01-01 00:33:16 +0200

    Update BFS python.py

--- BFS python.py
+++ BFS python.py
@@ -1,20 +1,18 @@
 def bfs(graph, node):
-    visited = [] # List to keep track of visited nodes.
+    visited = []  # List to keep track of visited nodes.
     queue = []
-
+    
     visited.append(node)
     queue.append(node)
 
     while queue:
-        s = queue.pop(0) 
-        print (s, end = " ") 
-        
-       for neighbour in graph[s]:
+        s = queue.pop(0)
+        print(s, end=" ")
+
+        for neighbour in graph[s]:
             if neighbour not in visited:
                 visited.append(neighbour)
                 queue.append(neighbour)
-
-bfs(graph, 'A')
 
 # O(V+E)
 # V = number of vertices
